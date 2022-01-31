55aaaef51974769aa8231a14d7c6fb595d4ea276
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-01-31 19:51:44 +0300

    update

--- App.js
+++ App.js
@@ -1,1 +1,33 @@
 import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet,Button } from 'react-native';
+
+
+function App() {
+  function sayHello(label) {
+    console.log('Merhabalar ' + label);
+
+
+  };
+  return (
+    <SafeAreaView >
+      <View style={styles.container}>
+        <Text>Hello World</Text>
+        <Button title="Hi" 
+        onPress={console.log('Hi')}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    margin: 10,
+    padding: 10,
+    borderRadius:5,
+
+  },
+});
+export default App;
