7f87e6c8d1b59a389976be36ac15308ee94b4693
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-02-02 19:37:04 +0300

    last update

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { View, Text, TouchableOpacity ,Alert} from 'react-native';
-import styles from './Card.style';
+import { Text, TouchableOpacity, View, Alert } from 'react-native'
+import styles from './card.style'
 
 const Card = (props) => {
     return (
-        <View style={styles.container}>
-            <View style={styles.body}>
-                <Text style={styles.title}>
-                    {props.title}
-                </Text>
-                <Text style={styles.text}>
-                    {props.quote}
-                </Text>
-            </View>
-            <TouchableOpacity
-                style={styles.button_container}
-                onPress={() => Alert.alert(props.title+ ' iyi demiş...')}>
-                <Text style={styles.button_text}>Hİİİ</Text>
-            </TouchableOpacity>
 
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                {props.title}
+            </Text>
+            <Text style={styles.text}>
+                {props.text}
+            </Text>
+            <View style={styles.button_container}>
+                <TouchableOpacity>
+                <Text style={styles.button_title}>
+                    LES GO
+                </Text>
+               </TouchableOpacity>
+            </View>
         </View>
 
     )
 }
-
-export default Card
+export default Card;
--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -1,54 +1,42 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
 
+container: {
 
-    container: {
-        backgroundColor: 'white',
-        margin: 10,
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 10,
-    }
-    ,
-    body: {
-        padding: 0,
+    backgroundColor: 'white',
+    margin:10,
+    borderWidth:1,
+    borderColor: 'gray',
+    borderRadius:10,
 
+},
 
-    },
+body: {
+    padding:10,
+},
+title:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin:10,
+    marginBottom:3,
 
-
-    title: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        margin: 10,
-        marginBottom: 1,
-
-
-    },
-    text: {
-        fontSize: 14,
-        margin: 10,
-        marginTop: 3,
-    },
-    button_container: {
-        backgroundColor: 'blue',
-        borderColor: 'white',
-        borderBottomLeftRadius: 10,
-        alignItems: 'center',
-        borderBottomRightRadius: 10,
-        padding: 10,
-
-
-    }
-    ,
-    button_text: {
-        color: 'white',
-        borderColor: 'white',
-        fontWeight: 'bold',
-        fontSize: 18,
-
-    }
-
+},
+text: {
+    fontSize:18,
+    margin:10,
+    marginTop:3,
+},
+button_container: {
+    backgroundColor: '#00C2FF',
+    alignItems: 'center',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+},
+button_title:{
+    fontSize : 18,
+    fontWeight : 'bold',
+    color: 'white',
+}
 
 })
